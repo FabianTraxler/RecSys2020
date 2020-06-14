9f7b77d2edec904b6332788dab82bb85eca05823
Fabsl22 <fabi.traxler@hotmail.com>
2020-06-14 18:41:04 +0200

    update

--- Matrix_Factorization.py
+++ Matrix_Factorization.py
@@ -13,14 +13,14 @@ from pyspark.mllib.evaluation import BinaryClassificationMetrics
 from pyspark.sql.functions import  when, col, rand, isnan
 
 
-conf = SparkConf().setAppName("RecSys-Challenge-2020").setMaster("yarn")
+conf = SparkConf().setAppName("RecSys-Challenge-Submission-Generation").setMaster("yarn")
 conf = (conf.set("deploy-mode","cluster")
-       .set("spark.driver.memory","100g")
-       .set("spark.executor.memory","100g")
+       .set("spark.driver.memory","50")
+       .set("spark.executor.memory","50")
        .set("spark.driver.cores","1")
-       .set("spark.num.executors","30")
+       .set("spark.num.executors","100")
        .set("spark.executor.cores","1")
-       .set("spark.driver.maxResultSize", "100g"))
+       .set("spark.driver.maxResultSize", "50"))
 
 sc = pyspark.SparkContext(conf=conf)
 sql = SQLContext(sc)
@@ -33,22 +33,13 @@ train_df = (sql.read
     .option("header", "false")
     .option("sep", "\x01")
     .load(datafile,  inferSchema="true")
-    .repartition(1000)
+    .limit(10)
+    #.repartition(1000)
     .toDF("text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains","tweet_type", "language", "tweet_timestamp", "engaged_with_user_id", "engaged_with_user_follower_count","engaged_with_user_following_count", "engaged_with_user_is_verified", "engaged_with_user_account_creation",\
                "engaging_user_id", "engaging_user_follower_count", "engaging_user_following_count", "engaging_user_is_verified","engaging_user_account_creation", "engaged_follows_engaging", "reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp"))
 
 train_df = train_df.select("engaging_user_id", "tweet_id", "reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp")
 
-
-#datafile_val = "hdfs:///user/pknees/RSC20/val.tsv"
-
-#test_df = (sql.read
-#    .format("csv")
-#    .option("header", "false")
-#    .option("sep", "\x01")
-#    .load(datafile_val,  inferSchema="true")
-#    .toDF("text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains","tweet_type", "language", "tweet_timestamp", "engaged_with_user_id", "engaged_with_user_follower_count","engaged_with_user_following_count", "engaged_with_user_is_verified", "engaged_with_user_account_creation",\
-#               "engaging_user_id", "engaging_user_follower_count", "engaging_user_following_count", "engaging_user_is_verified","engaging_user_account_creation", "engaged_follows_engaging"))
 
 tweet2id = train_df.select("tweet_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
 user2id = train_df.select("engaging_user_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
@@ -60,7 +51,7 @@ train_df = train_df.join(tweet2id, col("tweet_id") == col("tweet_id_str"))
 train_df = train_df.join(user2id, col("engaging_user_id") == col("user_id_str"))
 train_df = train_df.select("user", "tweet", "reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp")
 
-target_cols = ["like_timestamp"]
+target_cols = ["reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp"]
 
 def encode_response(x):
     return when(col(x).isNull(), float(0)).otherwise(float(1))
@@ -68,27 +59,56 @@ def encode_response(x):
 for target_col in target_cols:
     train_df = train_df.withColumn(target_col[:-10], encode_response(target_col))
 
-train_df = train_df.select("user", "tweet", "like")
+train_df = train_df.select("user", "tweet", "like","reply", "retweet", "retweet_with_comment" )
+
+
+datafile_val = "hdfs:///user/pknees/RSC20/val.tsv"
+
+val_df = (sql.read
+    .format("csv")
+    .option("header", "false")
+    .option("sep", "\x01")
+    .load(datafile_val,  inferSchema="true")
+    .limit(10)
+    .toDF("text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains","tweet_type", "language", "tweet_timestamp", "engaged_with_user_id", "engaged_with_user_follower_count","engaged_with_user_following_count", "engaged_with_user_is_verified", "engaged_with_user_account_creation",\
+               "engaging_user_id", "engaging_user_follower_count", "engaging_user_following_count", "engaging_user_is_verified","engaging_user_account_creation", "engaged_follows_engaging"))
+
+
+val_df = val_df.select("tweet_id","engaging_user_id")
+
+tweet2id_val = val_df.select("tweet_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
+user2id_val = val_df.select("engaging_user_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
+tweet2id_val = tweet2id_val.toDF().withColumnRenamed("_1", "tweet_id_str_val").withColumnRenamed("_2", "tweet_new")
+user2id_val = user2id_val.toDF().withColumnRenamed("_1", "user_id_str_val").withColumnRenamed("_2", "user_new")
+
+val_df = val_df.join(tweet2id_val, col("tweet_id") == col("tweet_id_str_val"), "left_outer")
+val_df = val_df.join(user2id_val, col("engaging_user_id") == col("user_id_str_val"), "left_outer")
+
+val_df = val_df.join(tweet2id, col("tweet_id") == col("tweet_id_str"), "left_outer")
+val_df = val_df.join(user2id, col("engaging_user_id") == col("user_id_str"), "left_outer")
+
+
+
 
 datafile_test = "hdfs:///user/pknees/RSC20/test.tsv"
-
 test_df = (sql.read
     .format("csv")
     .option("header", "false")
     .option("sep", "\x01")
     .load(datafile_test,  inferSchema="true")
+    .limit(10)
     .toDF("text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains","tweet_type", "language", "tweet_timestamp", "engaged_with_user_id", "engaged_with_user_follower_count","engaged_with_user_following_count", "engaged_with_user_is_verified", "engaged_with_user_account_creation",\
                "engaging_user_id", "engaging_user_follower_count", "engaging_user_following_count", "engaging_user_is_verified","engaging_user_account_creation", "engaged_follows_engaging"))
 
 test_df = test_df.select("tweet_id","engaging_user_id")
 
-tweet2id_new = test_df.select("tweet_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
-user2id_new = test_df.select("engaging_user_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
-tweet2id_new = tweet2id_new.toDF().withColumnRenamed("_1", "tweet_id_str_new").withColumnRenamed("_2", "tweet_new")
-user2id_new = user2id_new.toDF().withColumnRenamed("_1", "user_id_str_new").withColumnRenamed("_2", "user_new")
+tweet2id_test = test_df.select("tweet_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
+user2id_test = test_df.select("engaging_user_id").rdd.map(lambda x: x[0]).distinct().zipWithUniqueId()
+tweet2id_test = tweet2id_test.toDF().withColumnRenamed("_1", "tweet_id_str_test").withColumnRenamed("_2", "tweet_new")
+user2id_test = user2id_test.toDF().withColumnRenamed("_1", "user_id_str_test").withColumnRenamed("_2", "user_new")
 
-test_df = test_df.join(tweet2id_new, col("tweet_id") == col("tweet_id_str_new"), "left_outer")
-test_df = test_df.join(user2id_new, col("engaging_user_id") == col("user_id_str_new"), "left_outer")
+test_df = test_df.join(tweet2id_test, col("tweet_id") == col("tweet_id_str_test"), "left_outer")
+test_df = test_df.join(user2id_test, col("engaging_user_id") == col("user_id_str_test"), "left_outer")
 
 test_df = test_df.join(tweet2id, col("tweet_id") == col("tweet_id_str"), "left_outer")
 test_df = test_df.join(user2id, col("engaging_user_id") == col("user_id_str"), "left_outer")
@@ -129,7 +149,7 @@ for target_col in target_cols:
 
 
 def fallback_prediction(x):
-    return when(col(x).isNull(), rand()).otherwise(col(x))
+    return when(isnan(x), rand()).otherwise(col(x))
 
 for target_col in target_cols:
     target_col = target_col[:-10]
